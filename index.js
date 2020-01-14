41dd077525e5ed4d65549f06f74eea6c70101189
Steve-Sasaki <wmaaaa.sa@gmail.com>
2020-01-14 23:47:48 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const SerialPort = require('serialport');
+const port = new SerialPort("/dev/cu.usbmodem14101", {
+    parser: SerialPort.parsers.readline('\n'),
+    baudrate: 9600
+});
+
+port.on('open', function () {
+    console.log('Serial open.');
+    setInterval(write, 1000, 'OK\n');
+});
+
+port.on('data', function (data) {
+    console.log('Data: ' + data);
+});
+
+function write(data) {
+    // console.log('Write: ' + data);
+    port.write(new Buffer(data), function(err, results) {
+        if(err) {
+            console.log('Err: ' + err);
+            console.log('Results: ' + results);
+        }
+    });
+}
+
